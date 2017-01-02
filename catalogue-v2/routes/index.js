a0c346cf778bcf47b112da2207eede64d51616c0
Ben Hall <ben@benhall.me.uk>
2017-01-02 20:16:48 +0000

    Fix data

--- catalogue-v2/routes/index.js
+++ catalogue-v2/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  var data = [{name: 'Oasis', tickets: [{price: '12.00', seat: 'A1'}, {price: '12.00', seat: 'A2', data: '01/02/2017 18:00:00'}]}];
+  var data = [{name: 'Oasis', tickets: [{price: '12.00', seat: 'A1'}, {price: '12.00', seat: 'A2'}], date: '01/02/2017 18:00:00'}];
 
   res.json(data);
 });
---
+++ frontend/versions.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "catalogue": "v1.0.1"
+};
+
