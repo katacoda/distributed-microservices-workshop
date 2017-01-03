f3cb85822bae350792ed27c5efdb9e3c4f0db9b6
Ben Hall <ben@benhall.me.uk>
2017-01-03 11:51:08 +0000

    Move traffic to routing via Traefik

--- production/register.sh
+++ production/register.sh
@@ -1,3 +1,4 @@
+curl --upload-file register-80.json http://localhost:8500/v1/agent/service/register
 curl --upload-file register.json http://localhost:8500/v1/agent/service/register
 curl --upload-file register-v2.json http://localhost:8500/v1/agent/service/register
 
