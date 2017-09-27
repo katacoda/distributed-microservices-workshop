9366db1103e4e8549447c1264edadd0df6eeeb41
Ben Hall <ben@benhall.me.uk>
2017-09-27 12:21:56 +0200

    Refactoring

--- frontend/lib/getCatalogue.js
+++ frontend/lib/getCatalogue.js
@@ -2,10 +2,5 @@ var discover = require('./service-discovery');
 var request = require('./request');
 
 module.exports = function(cb) {
-  discover('catalogue', function(err, service) {
-    if(err) { return cb(err); }
-
-    service.path = "/";
-    request.get(service, cb);
-  });
+  request.get("http://localhost:8080", cb);
 };
